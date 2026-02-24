a370e4f8d4db5536337be26df4adfdc442595fc4
chataclaw <dev@quack.jp>
2026-02-24 18:43:30 +0900

    chore: scaffold Electron + React + Vite project

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,14 @@
+export default function App(): React.JSX.Element {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1>Budgie</h1>
+    </div>
+  )
+}
